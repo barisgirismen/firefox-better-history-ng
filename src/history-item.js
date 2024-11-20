4c3cb35790689aa92c15b28ac707499bc5066bf7
Christoph Wagner <gitlab@cwagner.me>
2024-11-20 17:10:54 +0100

    Allow opening all current links for a day in new tabs. Fixes #16

--- src/history-item.js
+++ src/history-item.js
@@ -5,31 +5,48 @@ const getLastVisit = (visit) =>
   'Last visit: ' + new Date(visit.lastVisitTime).toLocaleString(window.ihatereactngbetterhistoryregion);
 const getTitle = (visit) => (visit.title != null && visit.title !== '' ? visit.title : visit.url);
 
+function openTabs(urls) {
+  if (urls.length > 5 && !confirm(`This will open ${urls.length} new tabs, continue?`))
+    return;
+  urls.forEach(url => {
+    browser.tabs.create({ url: url });
+  });
+}
+
+const OpenAllLink = ({visits}) => (
+  <a className="openNewTab" title={`Open all ${visits.length} links in new tabs`} href="#" onClick={e => {
+    e.stopPropagation();
+    openTabs(visits.map((visit) => visit.url));
+  }}>{visits.length} Links</a>
+)
 export const History = ({ visits, day = false }) => (
   <div>
-    {visits.map((visit) => (
-      <div className="history-item">
-        {day ? (
-          <span className="history-item-date">
+    <OpenAllLink visits={visits} />
+    <div>
+      {visits.map((visit) => (
+        <div className="history-item">
+          {day ? (
+            <span className="history-item-date">
             {new Date(visit.lastVisitTime).toLocaleTimeString(window.ihatereactngbetterhistoryregion)}
           </span>
-        ) : (
-          ''
-        )}
+          ) : (
+            ''
+          )}
 
-        <div className="history-item-icon">
-          <Icon className="img-icon" default="globe" faviconUrl={visit.url} />
-        </div>
+          <div className="history-item-icon">
+            <Icon className="img-icon" default="globe" faviconUrl={visit.url} />
+          </div>
 
-        <span className="history-item-url ellipsis-text">
+          <span className="history-item-url ellipsis-text">
           <abbr title={getLastVisit(visit) + '; ' + getTitle(visit)}>{getTitle(visit)}</abbr>
         </span>
-        <div className="history-item-link">
-          <a href={visit.url} title={visit.url} target="_blank" rel="noopener noreferrer">
-            {day ? 'Link' : ''}
-          </a>
+          <div className="history-item-link">
+            <a href={visit.url} title={visit.url} target="_blank" rel="noopener noreferrer">
+              {day ? 'Link' : ''}
+            </a>
+          </div>
         </div>
-      </div>
-    ))}
-  </div>
-);
+      ))}
+    </div>
+    </div>
+    );
